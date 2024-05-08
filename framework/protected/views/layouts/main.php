3fbfaa2a10de5c98b3a3bfef23e4be2e333beb67
daffaalif88 <adaffa059@gmail.com>
2024-05-08 13:55:25 +0700

    update main

--- framework/protected/views/layouts/main.php
+++ framework/protected/views/layouts/main.php
@@ -24,7 +24,10 @@
 	<div class="container" id="page">
 
 		<div id="header">
-			<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+			<div id="logo">
+				<!-- <?php echo CHtml::encode(Yii::app()->name); ?> -->
+				Daffa Alif WengDev
+			</div>
 		</div><!-- header -->
 
 		<div id="mainmenu">
